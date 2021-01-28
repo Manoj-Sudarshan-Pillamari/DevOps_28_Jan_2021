7ef88a71c4a0327a901f46220c2da1abff9fd42f
Manoj-Sudarshan-Pillamari <69207398+Manoj-Sudarshan-Pillamari@users.noreply.github.com>
2021-01-28 14:27:13 +0530

    Update HellNo.java
    Changes from git

--- Demo1/src/com/cg/HellNo.java
+++ Demo1/src/com/cg/HellNo.java
@@ -5,7 +5,7 @@ public class HellNo {
 	public static void main(String[] args) {
 	System.out.println("HelloNo");
 		
-		System.out.println("From Github pulled");
+//		System.out.println("From Github pulled");
 
 	}
 	
